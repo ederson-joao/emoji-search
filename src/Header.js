b76eab4987018f876a3746672cef99fe19401416
Ederson João <ederson.joao@outlook.com>
2024-01-16 11:45:04 -0300

    feat: add icons to title

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,15 @@ export default class Header extends Component {
     render() {
         return (
             <header className="component-header">
+                <img className="icon-title"
+                    src="//cdn.jsdelivr.net/emojione/assets/png/1f50d.png"
+                    alt="Lupa Esquerda"
+                />
                 Emoji Search
+                <img className="icon-title"
+                    src="//cdn.jsdelivr.net/emojione/assets/png/1f50e.png"
+                    alt="Lupa Direita"
+                />
             </header>
         );
     }
